5090c079cac787d0b3b6fa072aa6f466315a224d
eunv <dmsql1091@naver.com>
2023-09-19 16:24:20 +0900

    List component create

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import List from './List';
 
-export default function List({ todoData, setTodoData }) {
+export default function Lists({ todoData, setTodoData }) {
 
 
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter(data => data.id !== id)
-    setTodoData(newTodoData);
-  };
-
-  const handleCompleChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
-  };
 
   const handleEnd = (result) => {
     //목적지가 없으면(이벤트 취소) 함수 종료
@@ -47,23 +34,16 @@ export default function List({ todoData, setTodoData }) {
                 todoData.map((data, index) => (
                   <Draggable key={data.id} draggableId={data.id.toString()} index={index}>
                     {(provided, snapshot) => (
-                      <div key={data.id}
-                        {...provided.draggableProps}
-                        ref={provided.innerRef}
-                        {...provided.dragHandleProps}
-                        className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rouneded`}>
-                          <div className='items-center'>
-                            <input
-                              type="checkbox"
-                              onChange={() => handleCompleChange(data.id)}
-                              defaultChecked={false} />
-                            <span className={data.completed ? 'line-through' : undefined} >{data.title}</span>
-                          </div>
-                          <div className='items-center'>
-                            <button className='px-4 py-1 float-right' onClick={() => handleClick(data.id)}>x</button>
-                          </div>
-                        </div>
+                      <List
+                        key={data.id}
+                        id={data.id}
+                        title={data.title}
+                        completed={data.completed}
+                        todoData={todoData}
+                        setTodoData={setTodoData}
+                        provided={provided}
+                        snapshot={snapshot}
+                      />
                     )}
 
                   </Draggable>
