ed90c5b3eab2f2fee40b34d10a376a90b314d266
eunv <dmsql1091@naver.com>
2023-09-19 16:40:30 +0900

    rendering optimization

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,19 +2,17 @@ import React from 'react'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import List from './List';
 
-export default function Lists({ todoData, setTodoData }) {
-
-
+const Lists = React.memo(({ todoData, setTodoData }) => {
 
   const handleEnd = (result) => {
     //목적지가 없으면(이벤트 취소) 함수 종료
-    if(!result.destination) return;
+    if (!result.destination) return;
 
     //리액트 불변성을 지켜주기 위해 새로운 todoData 생성
     const newTodoData = [...todoData];
 
     //변경시키는 아이템을 배열에서 지우고 return 값으로 지워진 아이템 잡기
-    const [reorderedItem] = newTodoData.splice(result.source.index,1);
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
 
     //원하는 자리에 reorderItem을 insert 해줌
     newTodoData.splice(result.destination.index, 0, reorderedItem);
@@ -22,9 +20,8 @@ export default function Lists({ todoData, setTodoData }) {
 
   };
 
+
   return (
-
-
     <div>
       <DragDropContext onDragEnd={handleEnd}>
         <Droppable droppableId="todo">
@@ -55,5 +52,7 @@ export default function Lists({ todoData, setTodoData }) {
       </DragDropContext>
 
     </div >
-  )
-}
+  );
+});
+
+export default Lists
